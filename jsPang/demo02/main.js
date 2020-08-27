7def59779b10ac59fbf4b2c969f1d1a7d58c15a2
xiaoxiaoweii <2679656468@qq.com>
2020-08-27 23:17:43 +0800

    jsoang-fs electron

---
+++ jsPang/demo02/main.js
@@ -0,0 +1,20 @@
+var electron = require('electron');
+var app = electron.app; // 引用app
+var BrowserWindow = electron.BrowserWindow; // 窗口引用
+
+var mainWindow = null; // 声明要打开的主窗口
+
+app.on('ready', () => {
+	(mainWindow = new BrowserWindow({
+		width: 800,
+		height: 800,
+		webPreferences: {
+			/* 启用node下边的所有功能 */
+			nodeIntegration: true,
+		},
+	})),
+		mainWindow.loadFile('index.html'); // 加载html页面
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+});
